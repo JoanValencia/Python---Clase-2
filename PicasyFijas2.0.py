19dd546319349d46908e9ee8a041a9196bcbaf35
JoanValencia <52727228+JoanValencia@users.noreply.github.com>
2019-07-11 21:44:59 -0500

    Add files via upload

--- PicasyFijas2.0.py
+++ PicasyFijas2.0.py
@@ -6,12 +6,38 @@ fn picas
 fn fijas
 mostrar resultados en matrix [num, picas, fijas]
 """
+# Librerías:
 from random import randint
 
+# Declaración de variables:
 i = 0
+intent = 0
 listnum = []
+lista = []
 val = False
 tam = 3
+pica = 0
+fija = 0
+arreglo = []
+
+
+# Funciones:
+def solicitud(val):
+    while val == False:
+        num = raw_input("Ingrese un numero de tres digitos: ")
+        listnum = [int(i) for i in list(num)]
+        val = validacion(listnum)
+    return listnum
+
+def validacion(listnum):
+    if len(listnum) == 3:
+        return True
+    if len(listnum) > 3:       
+        print "Ha ingresado mas de tres digitos"
+        return False
+    if len(listnum) < 3:      
+        print "Ha ingresado menos de tres digitos"
+        return False
 
 def generar_aleatorios(tam):
     lista = []
@@ -21,36 +47,8 @@ def generar_aleatorios(tam):
             lista.append(numero)
     return lista
 
-def solicitud(val):
-    if val == False:
-        num = raw_input("Ingrese un numero de tres digitos: ")
-        listnum = [int(i) for i in list(num)]
-    return listnum
-
-def validacion(listnum):
-    if len(listnum) == 3:
-        val = True
-    elif len(listnum) > 3:
-        val = False
-        print "Ha ingresado mas de tres digitos"
-        solicitud(val)
-    else:
-        val = False
-        print "Ha ingresado menos de tres digitos"
-        solicitud(val)
-    return val
-
-#def comparacion(gen,usr):
-#    for i in lista 
-
-listnum = solicitud(val)
-print listnum
-print generar_aleatorios(tam)
-print validacion(listnum)
-#print picas(lista,listnum)
-
-"""
 def comparar(list1,list2):
+#    print list1, list2
     pica = 0
     fija = 0
     for i in range(len(list1)):
@@ -59,4 +57,26 @@ def comparar(list1,list2):
                 fija += 1
             else:
                 pica += 1
-"""             
+    return pica, fija
+
+
+    
+    
+# Ejecución:              
+listnum = solicitud(val)
+lista = generar_aleatorios(tam)
+pica, fija = comparar(listnum,lista)
+print "Tienes " + str(pica) + " pica(s) y " + str(fija) + " fija(s)" 
+arreglo.append([listnum,pica,fija])
+
+# Pruebas:
+print ""
+print arreglo
+print pica , fija   
+print listnum
+print lista
+val = validacion(listnum)
+print val
+
+#https://github.com/apdaza/python_samples/tree/master/picas%20y%20fijas
+           
